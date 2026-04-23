b19fba5356e8f7e191aa0488a64fee26208e6797
Roman2011Kiber <100216858+Roman2011Kiber@users.noreply.github.com>
2026-04-23 09:44:09 +0300

    Update Firebase configuration with new credentials

--- mafia/config.js
+++ mafia/config.js
@@ -13,12 +13,13 @@
 // =====================================================================
 
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+    apiKey: "AIzaSyBAtxUHSyMNjzHJLzpC-T_3sdgDVo5T6qI",
+    authDomain: "js25-52181.firebaseapp.com",
+    projectId: "js25-52181",
+    storageBucket: "js25-52181.appspot.com",
+    messagingSenderId: "948851257740",
+    appId: "1:948851257740:web:c1310e73adae048235e6a9",
+    measurementId: "G-K77YQN0D58"
 };
 
 export const isFirebaseConfigured =
